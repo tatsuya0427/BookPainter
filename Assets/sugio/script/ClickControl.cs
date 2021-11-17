387b63a15cd25f38cd8a24e97e87faf2de3080a2
杉尾達也 <sugitatsu0427.rimi@gmail.com>
2021-11-18 00:25:03 +0900

    色変え機能
    以前のプロジェクトの色変更の実装を今回のゲームの内容に合うように変更

---
+++ Assets/sugio/script/ClickControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClickControl : ColorStorage{
+    GameObject clickObject;//画面をクリックした際に、Rayを飛ばし、Rayにぶつかったオブジェクトを格納しておく
+    [SerializeField] static colorType setClickColor;//CanChangeColorObjectTemplateを継承したスクリプトを持つオブジェクトをクリックした際にここに設定されている色に変更する
+
+    void Update(){
+        if(!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()　&& Input.GetMouseButtonDown(0)){//マウスの左クリックを押した際にRayを飛ばして当たったオブジェクトを取得する
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            
+            Debug.Log(Physics.RaycastAll(ray));
+ 
+            if (Physics.Raycast(ray, out hit)) {
+                clickObject = hit.collider.gameObject;
+            }
+
+            //オブジェクトを取得できた＆tagがcanChangeColor出会った場合に、そのオブジェクトの持つ色を自身の持つ色(ClickControl.csのnowColor)に変更する
+            if(clickObject != null && !(clickObject.CompareTag("UIObject"))){//ここでUIObjectを持つタグは除外している理由として、UIをクリックした際に誤作動を起こさないため。そのためクリックするUIにはUIObjectのタグ付けをする必要がある
+                if(clickObject.CompareTag("canChangeColor")){
+                    clickObject.GetComponent<CanChangeColorObjectTemplate>().SetObjectColor(setClickColor);
+                }
+            }
+        }
+    }
+
+    protected internal void SetClickControlColor(colorType targetColor){//このクラスが継承されているクラスが所持する色(nowColor)を変更するメソッド
+            //setClickColor = (colorType)Enum.ToObject(typeof(colorType), targetColor);
+            setClickColor = targetColor;
+            Debug.Log(setClickColor　+ "色を装備しました");
+    }
+}
---
+++ Assets/sugio/script/ColorStorage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ColorStorage : MonoBehaviour{
+    protected internal enum colorType{//色の種類を列挙型で定義しておく。(間違った値が入らないように)このクラスを継承したクラスを使用し、色の管理はこの列挙型で行う。
+            white,
+            black,
+            red,
+            blue,
+            yellow,
+            green
+
+    }
+}
+
