4928c2ce6de20fb2c5638871227cfbc4e89575da
takenokogohan <bambootreeku@gmail.com>
2021-11-24 15:17:11 +0900

    タイトル画面を追加しました。

--- Assets/Sasaki/Scripts/SceneManagerScript.cs
+++ Assets/Sasaki/Scripts/SceneManagerScript.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Image panelImage;
+    private float alpha = 0f;
+    private float speed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,11 +25,25 @@ public class SceneManagerScript : MonoBehaviour
 
     public void toGameScene()
     {
-        SceneManager.LoadScene("GameUIScene");
+        panel.SetActive(true);
+        StartCoroutine(SceneMove());
     }
 
     public void toTitleScene()
     {
         SceneManager.LoadScene("TitleScene");
+    }
+
+    IEnumerator SceneMove()
+    {
+        Debug.Log("ゲームシーンへ移動します");
+        while (alpha < 1f)
+        {
+            alpha += speed;
+            panelImage.color = new Color(0f, 0f, 0f, alpha);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("GameUIScene");
     }
 }
---
+++ Assets/Sasaki/Scripts/TitleManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleManagerScript : MonoBehaviour
+{
+    private Image startButtonImage;
+    private float alpha = 0f;
+    private float speed = 1.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButtonImage = GameObject.Find("StartButton").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alpha += speed * Time.deltaTime;
+        startButtonImage.color = new Color(1f, 1f, 1f, 0.6f*Mathf.Abs(Mathf.Sin(alpha)));
+    }
+}
