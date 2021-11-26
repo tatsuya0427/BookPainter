bec573d2682cf43b9bc870b57b0736f953d30f9c
takenokogohan <51778676+takenokogohan@users.noreply.github.com>
2021-11-26 19:53:00 +0900

    ボタン選択時のエフェクトを追加

---
+++ Assets/Sasaki/Scripts/ButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EventOn()
+    {
+        this.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    public void EventOff()
+    {
+        this.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+    }
+}
